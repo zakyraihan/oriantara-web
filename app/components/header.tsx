e9bef2d499cc37db5589b20e1cf2cf1cb7697747
zaky <zakyraihan6336@gmail.com>
2025-12-14 23:08:43 +0800

    tes

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -184,7 +184,7 @@ export default function OriantaraLanding() {
 
                     <button
                         onClick={() => router.push("/brosur")}
-                        className="px-6 py-2 bg-gray-600 animate-pulse text-white rounded-full"
+                        className="px-6 py-2 bg-gradient-to-r from-gray-600 to-gray-300 hover:from-gray-700 hover:to-gray-400 text-white rounded-full transition-all duration-300"
                     >
                         Download Brosur
                     </button>
